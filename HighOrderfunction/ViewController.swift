c3c7e6f703b65a8ead933f8081f4bdcd0bdce19a
Deepankar <deepankar1992p@gmail.com>
2021-05-24 12:50:12 +0530

    Initial Commit

---
+++ HighOrderfunction/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HighOrderfunction
+//
+//  Created by Deepankar  on 24/05/21.
+//  Copyright © 2021 Deepankar Parashar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
